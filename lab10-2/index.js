916a5c2795b05c84332683b85a68d6539ed194a8
Kartpol123 <kartpol47@gmail.com>
2025-02-14 21:17:23 +0700

    lab10-2
    -- complete --

---
+++ lab10-2/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+const { CLIENT_RENEG_LIMIT } = require("tls");
+const port = 3000;
+const sqlite3 = require('sqlite3').verbose();
+
+// Creating the Express server
+const app = express();
+
+// Connect to SQLite database
+let db = new sqlite3.Database('questions.db', (err) => {    // if exist open / if not exist create
+  if (err) {
+      return console.error(err.message);
+  }
+  console.log('Connected to the SQlite database.');
+});
+
+// static resourse & templating engine
+app.use(express.static('public'));
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
+
+
+// routing path
+app.get('/', function(req, res){
+    const sql = `select * from questions`
+    db.all(sql, (err, rows) => {
+        if (err){
+            console.log(err)
+        }
+        res.render('show', {data : rows});
+    })
+})
+
+
+// Starting the server
+app.listen(port, () => {
+   console.log("Server started.");
+ });
